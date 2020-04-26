2c75b033eb6194ef8402f64188d11db2a35fa57f
Nahuel Veron <nahuelav96@gmail.com>
2020-04-26 14:29:48 -0300

    Adding support for other images handlers, adding url-loader

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function withSVGComponent(baseConfig = {}) {
   return Object.assign({}, baseConfig, {
     webpack(config, options) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [require.resolve('@svgr/webpack'), require.resolve('file-loader')]
-      });
+      
+      config.module.rules = [
+        ...config.module.rules.map(_ => {
+          if ( _.test && _.test.toString().includes('svg|')) {
+            return {
+              ..._,
+              test: new RegExp(_.test.source.replace('svg|', ''))
+            }
+          }
+          return _
+        }),
+        {
+          test: /\.svg$/,
+          use: [require.resolve('@svgr/webpack'), {
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: baseConfig.inlineImageLimit,
+              fallback: require.resolve("file-loader"),
+              publicPath: `${baseConfig.assetPrefix}/_next/static/images/`,
+              outputPath: `${options.isServer ? "../" : ""}static/images/`,
+              name: "[name]-[hash].[ext]",
+              esModule: baseConfig.esModule || false
+            }
+          }]
+        }
+      ]
 
       if (typeof baseConfig.webpack === "function") {
         return baseConfig.webpack(config, options);
